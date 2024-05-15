96157205d78ed6365cd3038268cb55a30716cdd7
Anusha Salman <anushasalman@gmail.com>
2024-05-15 11:47:38 -0500

    functioning without console error

--- store/index.js
+++ store/index.js
@@ -1,9 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./authSlice.js"
+
+// // Handles different action types and update the state accordingly
+// const reducer = (state = initialState, action) => {
+
+// };
 
 const store = configureStore({
   reducer: {
-
+    auth: authReducer,
   },
 });
 
-export default store
+export default store;
