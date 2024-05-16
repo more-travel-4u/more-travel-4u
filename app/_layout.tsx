5b093286647f14e7821276ca21f8a1410f6e9fdc
Anusha Salman <anushasalman@gmail.com>
2024-05-16 10:37:46 -0500

    working on navbar, not completed yet

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,43 @@
 import { Stack } from "expo-router";
 import store from '../store/index.js';
 import { Provider } from 'react-redux';
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 export default function RootLayout() {
 
   return (
     <>
-      <Provider store={store}>
-        <Stack>
-          <Stack.Screen name="index" />
-        </Stack>
-      </Provider>
+      {/* <NavigationContainer> */}
+      <Tab.Navigator>
+        {/* <Provider store={store}> */}
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+        {/* </Provider> */}
+      </Tab.Navigator>
+      {/* </NavigationContainer> */}
     </>
   );
 }
+
+
