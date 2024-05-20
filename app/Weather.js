ec02b3970568bce02f5ee459b314a805d4e24c6b
jkl <alexwang925@gmail.com>
2024-05-20 13:45:33 -0300

    created Weather.js

---
+++ app/Weather.js
@@ -0,0 +1,17 @@
+import { useEffect } from 'react';
+import { View, Text } from 'react-native';
+
+const Weather = () => {
+
+  useEffect(() => {
+
+  }, [],)
+  
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default Weather
