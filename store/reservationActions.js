e290b22dd77c2469d73632199f623c6ca2d266df
Elijah Thode <elijahthode@gmail.com>
2024-05-23 09:12:25 -0500

    Continuing the build of the reservation

--- store/reservationActions.js
+++ store/reservationActions.js
@@ -1,5 +1,5 @@
-export const SET_RESERVATION = 'SET_RESERVATION';
-export const UPDATE_RESERVATION = 'UPDATE_RESERVATION';
+const SET_RESERVATION = 'SET_RESERVATION';
+const UPDATE_RESERVATION = 'UPDATE_RESERVATION';
 
 export const setReservation = (reservation) => ({
   type: SET_RESERVATION,
