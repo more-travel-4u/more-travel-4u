d0e3956ab31872f4a32fca618203808316055b93
jkl <alexwang925@gmail.com>
2024-05-24 13:46:18 -0300

    datepicker added

--- app/Planner.js
+++ app/Planner.js
@@ -1,15 +1,62 @@
-import { Text, View } from 'react-native';
 import FloatingAdd from './FloatingAdd.js';
-import { useEffect } from 'react';
+import { useState } from 'react';
+import { Button } from 'react-native';
+// import { Button, Text } from 'react-native-paper';
+import { Text } from 'react-native-paper';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { SafeAreaView, View } from 'react-native';
 
 export default function Planner({ navigation }) {
 
   return (
     <>
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <DatePicker />
         <Text>Planner!</Text>
       </View>
       <FloatingAdd {...{navigation}}/>
     </>
   );
 }
+
+const DatePicker = () => {
+  const [date, setDate] = useState(new Date());
+  const [mode, setMode] = useState('date');
+  const [show, setShow] = useState(false);
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate;
+    setShow(false);
+    setDate(currentDate);
+  };
+
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
+  const showDatepicker = () => {
+    showMode('date');
+  };
+
+  // const showTimepicker = () => {
+  //   showMode('time');
+  // };
+
+  return (
+    <SafeAreaView>
+      <Button onPress={showDatepicker} title="Show date picker!" />
+      {/* <Button onPress={showTimepicker} title="Show time picker!" /> */}
+      <Text>selected: {date.toLocaleString()}</Text>
+      {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={date}
+          mode={mode}
+          is24Hour={true}
+          onChange={onChange}
+        />
+      )}
+    </SafeAreaView>
+  );
+};
