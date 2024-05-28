389250ebead60b7cef174d4dccb15563c802cb65
jkl <alexwang925@gmail.com>
2024-05-28 17:14:16 -0300

    fix main breaking

--- store/weatherSlice.js
+++ store/weatherSlice.js
@@ -3,17 +3,22 @@ import { createSlice } from "@reduxjs/toolkit";
 export const weatherSlice = createSlice({
   name: 'weather',
   initialState: {
-    currentWeather: null
+    currentWeather: null,
+    errorMessage: "",
   },
   reducers: {
     setCurrentWeather: (state, action) => {
       state.currentWeather = action.payload;
+    },
+    setErrorMessage: (state, action) => {
+      state.errorMessage = action.payload;
     }
   }
 })
 
 export const {
   setCurrentWeather,
+  setErrorMessage
 } = weatherSlice.actions;
 
 export default weatherSlice.reducer;
