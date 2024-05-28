f16398100abf4c0782ee44db79ac977e3e293612
Elijah Thode <elijahthode@gmail.com>
2024-05-28 11:56:31 -0500

    Forgot I was on main commmented out Register.js to test

--- app/Register.js
+++ app/Register.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { useState, useEffect } from 'react';
 import { View, Text, TextInput, Button, StyleSheet, Alert } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
@@ -5,9 +6,18 @@ import { setUsername, setPassword, setEmail, clearAuth, setToken, setAuthMessage
 import * as SecureStore from 'expo-secure-store'
 import { ActivityIndicator, MD2Colors } from "react-native-paper";
 import { API_URL } from './Login.js';
+=======
+// import { useState, useEffect } from 'react';
+// import { View, Text, TextInput, Button, StyleSheet, Alert } from 'react-native';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { setUsername, setPassword, setEmail, clearAuth, setToken, setAuthMessage } from './../store/authSlice.js';
+// import * as SecureStore from 'expo-secure-store'
+// import { API_URL } from './Login.js';
+>>>>>>> e4f322b (Forgot I was on main commmented out Register.js to test)
 
-const Register = ({ navigation }) => {
+// const Register = ({ navigation }) => {
 
+<<<<<<< HEAD
   const dispatch = useDispatch();
   const [isLoading, setIsLoading] = useState(false);
   const authMessage = useSelector(state => state.auth.authMessage)
@@ -16,29 +26,39 @@ const Register = ({ navigation }) => {
     email: '',
     password: ''
   });
+=======
+//   const dispatch = useDispatch();
+//   const authMessage = useSelector(state => state.auth.authMessage)
+//   const [formData, setFormData] = useState({
+//     username: '',
+//     email: '',
+//     password: ''
+//   });
+>>>>>>> e4f322b (Forgot I was on main commmented out Register.js to test)
 
-  useEffect(() => {
-    return (() => {
-      dispatch(setAuthMessage(""))
-      setFormData({
-        username: '',
-        email: '',
-        password: ''
-      })
-  })
-  }, [],)
+//   useEffect(() => {
+//     return (() => {
+//       dispatch(setAuthMessage(""))
+//       setFormData({
+//         username: '',
+//         email: '',
+//         password: ''
+//       })
+//   })
+//   }, [],)
 
-  const handleChange = (name, value) => {
-    setFormData(prevState => ({
-      ...prevState,
-      [name]: value
-    }));
-  };
+//   const handleChange = (name, value) => {
+//     setFormData(prevState => ({
+//       ...prevState,
+//       [name]: value
+//     }));
+//   };
 
-  const handleRegister = async (event) => {
-    event.preventDefault();
-    const { username, email, password } = formData;
+//   const handleRegister = async (event) => {
+//     event.preventDefault();
+//     const { username, email, password } = formData;
 
+<<<<<<< HEAD
     // Basic validation
     if (!username || !email || !password) {
       Alert.alert('Validation Failed', 'All fields are required.');
@@ -117,27 +137,98 @@ const Register = ({ navigation }) => {
     </View>
   );
 };
+=======
+//     // Basic validation
+//     if (!username || !email || !password) {
+//       Alert.alert('Validation Failed', 'All fields are required.');
+//       return;
+//     }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16,
-    justifyContent: 'center'
-  },
-  title: {
-    fontSize: 24,
-    marginBottom: 16,
-    textAlign: 'center'
-  },
-  inputContainer: {
-    marginBottom: 12
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: '#ccc',
-    padding: 8,
-    borderRadius: 4
-  }
-});
+//     try {
+//       const response = await fetch(API_URL + "/auth/register", {
+//         method: "POST",
+//         headers: {
+//           "Accept": "application/json",
+//           "Content-Type": "application/json"
+//         },
+//         body: JSON.stringify(formData)
+//       });
+//       const data = await response.json();
+//       if (response.ok) {
+//         dispatch(setToken(data.token));
+//         await SecureStore.setItemAsync("token", data.token);
+//         setFormData({
+//           username: '',
+//           email: '',
+//           password: ''
+//         })
+//       } else {
+//         dispatch(setAuthMessage(data.message || "Registration failed. Please try again later."))
+//       }
+//     } catch (error) {
+//       dispatch(setAuthMessage("Network error. Please try again later."))
+//       console.error('Registration Error:', error);
+//     }
+//   };
 
-export default Register;
+//   return (
+//     <View style={styles.container}>
+//       {authMessage && <Text>{authMessage}</Text>}
+//       <Text style={styles.title}>Register</Text>
+//       <View style={styles.inputContainer}>
+//         <Text>Username:</Text>
+//         <TextInput
+//           style={styles.input}
+//           value={formData.username}
+//           onChangeText={(text) => handleChange('username', text)}
+//         />
+//       </View>
+//       <View style={styles.inputContainer}>
+//         <Text>Email:</Text>
+//         <TextInput
+//           style={styles.input}
+//           value={formData.email}
+//           onChangeText={(text) => handleChange('email', text)}
+//           keyboardType="email-address"
+//         />
+//       </View>
+//       <View style={styles.inputContainer}>
+//         <Text>Password:</Text>
+//         <TextInput
+//           style={styles.input}
+//           value={formData.password}
+//           onChangeText={(text) => handleChange('password', text)}
+//           secureTextEntry={true}
+//         />
+//       </View>
+//       <Button title="Register" onPress={handleRegister} />
+//       <Text>Already have an account?</Text>
+//       <Button title="Login Here" onPress={() => navigation.navigate("Login")} />
+//     </View>
+//   );
+// };
+>>>>>>> e4f322b (Forgot I was on main commmented out Register.js to test)
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     padding: 16,
+//     justifyContent: 'center'
+//   },
+//   title: {
+//     fontSize: 24,
+//     marginBottom: 16,
+//     textAlign: 'center'
+//   },
+//   inputContainer: {
+//     marginBottom: 12
+//   },
+//   input: {
+//     borderWidth: 1,
+//     borderColor: '#ccc',
+//     padding: 8,
+//     borderRadius: 4
+//   }
+// });
+
+// export default Register;
