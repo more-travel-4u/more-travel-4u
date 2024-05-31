135c7dbc18ffebbe648ac92ddab97317e6a70135
jkl <alexwang925@gmail.com>
2024-05-31 11:59:43 -0300

    added documentation

--- utils.js
+++ utils.js
@@ -1,14 +1,25 @@
-// Function for formatting our date. // takes in .toISOString(); of Date obj
+/**
+ * Function for formatting date from Date object.
+ * @param {string} inputDate - format is the output of .toISOString() on a Date object.
+ * @returns {string} Returns in this format: "Friday, May 31, 2024"
+ */
 export const formatDate = (inputDate) => {
   const date = inputDate.slice(0, 10)
+  const dateObj = new Date(inputDate);
+  const day = dateObj.getDay();
+  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
   const months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
   const strArray = date.split('');
   let monthNum = strArray[5] + strArray[6];
   if (monthNum[0] === '0') monthNum = monthNum.substring(1);
-  return `${months[Number(monthNum)-1]} ${strArray[8]}${strArray[9]}, ${strArray[0]}${strArray[1]}${strArray[2]}${strArray[3]}`
+  return `${days[day]}, ${months[Number(monthNum)-1]} ${strArray[8]}${strArray[9]}, ${strArray[0]}${strArray[1]}${strArray[2]}${strArray[3]}`
 }
 
-// Function for formatting time. // takes in .toISOString(); of Date obj
+/**
+ * Function for formatting time from Date object.
+ * @param {string} inputDate - format is the output of .toISOString() on a Date object.
+ * @returns {string} Returns in this format: "1:35 PM"
+ */
 export const formatTime = (inputDate) => {
   const time = inputDate.slice(11, 16)
   const timeArray = time.split("");
